90970b419abaecaaecdfcae1d3f614656f59604e
Stjepan Poljak <spoljak@seahost.dominion>
2018-04-23 21:47:06 +0200

    Selecting cells with keyboard.

--- TicTacToeGame/TicTacToe.hs
+++ TicTacToeGame/TicTacToe.hs
@@ -4,7 +4,7 @@ import Control.Monad.State
 import System.IO
 
 gOrigin :: (Int, Int)
-gOrigin = (10,1)
+gOrigin = (10, 1)
 
 gWidth :: Int
 gWidth = 3
@@ -27,7 +27,7 @@ main = do
     hSetEcho stdout False
 
     drawAll
-    waitForInput []
+    waitForInput [] (PX0, PY0)
     
     showCursor
     clearScreen
@@ -42,7 +42,7 @@ drawAll = do
 
     height <- drawBoard gOrigin gWidth gHeight gPaddingX gPaddingY
     
-    currPos <- printStringOn (screenPos gOrigin (PX2,PY1)
+    currPos <- printStringOn (screenPos gOrigin (PX2, PY1)
                                         gWidth gHeight
                                         gPaddingX gPaddingY) "#" False
     
@@ -50,45 +50,80 @@ drawAll = do
     
     putStrLn $ "\ESC[" ++ (show prevY) ++ ";0H"
 
-    logDown ("drawing board: O" ++ (show gOrigin)
-                                ++ " dim "
-                                ++ (show $ length [(PX0)..])
-                                ++ "×"
-                                ++ (show $ length [(PY0)..])
-                                ++ " each "
-                                ++ (show gWidth)
-                                ++ "×"
-                                ++ (show gHeight)
-                                ++ " pad "
-                                ++ (show gPaddingX)
-                                ++ "×"
+    logDown ("drawing board: O" ++ (show gOrigin) ++ " dim "
+                                ++ (show $ length [(PX0)..]) ++ "×"
+                                ++ (show $ length [(PY0)..]) ++ " each "
+                                ++ (show gWidth) ++ "×"
+                                ++ (show gHeight) ++ " pad "
+                                ++ (show gPaddingX) ++ "×"
                                 ++ (show gPaddingY)
                                 ++ " (press ESC to quit)") prevY
 
         -- **************************** KEYBOARD **************************** --
 
-waitForInput :: [Char] -> IO ()
-waitForInput prev = do
+waitForInput :: [Char] -> (TPosX, TPosY) -> IO ()
+waitForInput prev pos = do
+    
      x <- stdin `ifReadyDo` getChar
-     takeAction x prev
+     takeAction x prev pos
 
-takeAction :: Maybe Char -> [Char] -> IO ()
-takeAction Nothing prev
+move :: Direction -> (TPosX, TPosY) -> (Int, Int) -> Int -> Int -> Int -> Int -> IO (TPosX, TPosY)
+move dir pos origin width height padX padY = do
+
+                                    logDown (show dir) 0
+
+                                    drawBox realPos width height
+                                            padX padY (isTopRow pos)
+                                            False
+
+                                    drawBox newPos width height
+                                            padX padY (isTopRow new)
+                                            True
+
+                                    return new
+        
+        where realPos = screenPos gOrigin pos gWidth gHeight gPaddingX gPaddingY
+              newPos = screenPos origin new width height padX padY
+              new = (go pos dir)
+
+moveConv :: Direction -> (TPosX, TPosY) -> IO (TPosX, TPosY)
+moveConv dir pos = move dir pos gOrigin gWidth gHeight gPaddingX gPaddingY
+
+takeAction :: Maybe Char -> [Char] -> (TPosX, TPosY) -> IO ()
+takeAction Nothing prev pos
 
         | prev == ['\ESC']           = return ()
-        | prev == ['A','[','\ESC']   = do { logDown "Up!" 0; waitForInput [] }
-        | prev == ['B','[','\ESC']   = do { logDown "Down!" 0; waitForInput [] }
-        | prev == ['C','[','\ESC']   = do { logDown "Right!" 0; waitForInput [] }
-        | prev == ['D','[','\ESC']   = do { logDown "Left!" 0; waitForInput [] }
-        | prev == ['\n']             = do { logDown "Enter!" 0; waitForInput [] }
-        | otherwise                  = waitForInput []
+        | prev == ['A','[','\ESC']   = waitForInput [] =<< moveConv DUp pos
+        | prev == ['B','[','\ESC']   = waitForInput [] =<< moveConv DDown pos
+        | prev == ['C','[','\ESC']   = waitForInput [] =<< moveConv DRight pos
+        | prev == ['D','[','\ESC']   = waitForInput [] =<< moveConv DLeft pos
+        | prev == ['\n']             = do { logDown "Enter!" 0; waitForInput [] pos }
+        | otherwise                  = waitForInput [] pos
 
-takeAction (Just char) prev = waitForInput (char:prev)
+takeAction (Just char) prev pos = waitForInput (char:prev) pos
 
 ifReadyDo :: Handle -> IO a -> IO (Maybe a)
 ifReadyDo hnd x = hReady hnd >>= f
    where f True = x >>= return . Just
          f _    = return Nothing
+
+incX :: TPosX -> TPosX
+incX x = if x == last [(PX0)..] then x else succ x
+
+incY :: TPosY -> TPosY
+incY y = if y == PY0 then y else pred y
+
+decX :: TPosX -> TPosX
+decX x = if x == PX0 then x else pred x
+
+decY :: TPosY -> TPosY
+decY y = if y == last [(PY0)..] then y else succ y
+
+go :: (TPosX, TPosY) -> Direction -> (TPosX, TPosY)
+go (x, y) DUp = (x, incY y)
+go (x, y) DRight = (incX x, y)
+go (x, y) DLeft = (decX x, y)
+go (x, y) DDown = (x, decY y)
 
         -- ***************************** TERMINAL ***************************** --
 
@@ -128,58 +163,52 @@ putChars rep char
       | rep > 0         = char:putChars (rep - 1) char
       | otherwise       = ""
 
-printBoxPart :: (Int, Int) -> Int -> Int -> Bool -> Bool -> Int -> Bool -> IO Int
-printBoxPart (x, y) width height hasYPadding isTopRow currLevel selected =
-    
-    if currLevel == height + 1 then do return currLevel
+printBoxPart :: (Int, Int) -> Int -> Int -> Int -> Int -> Bool -> Int -> Bool -> IO Int
+printBoxPart (x, y) width height padX padY isTopRow currLevel selected =
 
-    else do printStringOn (x, (y+currLevel))
-                          (hBound:(putChars (width * 2 - 1) wBound)
-                       ++ [hBound]) selected
+    if currLevel == height + 1 then return currLevel else
 
-            return =<< printBoxPart (x,y) width height
-                                    hasYPadding isTopRow
-                                    (currLevel + 1)
-                                    selected
+      if currLevel == 0 && selected && not isTopRow then do
 
-    where hBound = if currLevel == 0 && (hasYPadding || isTopRow) then ' ' else '|'
+        putStrLn $ "\ESC[" ++ (show (y+currLevel)) ++ ";" ++ (show x) ++ "H|\ESC[1m"
+                                                   ++ (putChars (width * 2 - 1) wBound)
+                                                   ++ "\ESC[m|"
+        return =<< printBoxPart (x,y) width height padX padY isTopRow (currLevel + 1) selected
+
+      else do
+        
+        printStringOn (x, (y+currLevel))
+                      (hBound:(putChars (width * 2 - 1) wBound)
+                   ++ [hBound]) selected
+        return =<< printBoxPart (x,y) width height padX padY isTopRow (currLevel + 1) selected
+
+    where hBound = if currLevel == 0 && ((padY /= 0) || isTopRow) then ' ' else '|'
           wBound = if currLevel `elem` [0, height] then '_' else ' '
 
 sub :: (Int, Int) -> (Int, Int) -> (Int, Int)
 (x1, y1) `sub` (x2, y2) = (x1 - x2, y1 - y2)
 
-drawBox :: (Int, Int) -> Int -> Int -> Bool -> Bool -> Bool -> IO Int
-drawBox (x, y) width height hasYPadding isTopRow selected = do
+drawBox :: (Int, Int) -> Int -> Int -> Int -> Int -> Bool -> Bool -> IO Int
+drawBox (x, y) width height padX padY isTopRow selected = do
 
-                                                     newY <- printBoxPart (x, y)
-                                                                          width
-                                                                          height
-                                                                          hasYPadding
-                                                                          isTopRow 0
-                                                                          selected
-                                                     return (newY + y - 1)
+                                                   newY <- printBoxPart (x, y) width height
+                                                                        padX padY isTopRow 0
+                                                                        selected
+                                                   return (newY + y - 1)
 
 printStringOn :: (Int, Int) -> String -> Bool -> IO Int
-printStringOn (x, y) string bold = do putStrLn $ "\ESC[" ++ (show y)
-                                                         ++ ";"
-                                                         ++ (show x)
-                                                         ++ "H"
-                                                         ++ escBoldStart
-                                                         ++ string
-                                                         ++ escBoldEnd
+printStringOn (x, y) string bold = do putStrLn $ "\ESC[" ++ (show y) ++ ";" ++ (show x) ++ "H"
+                                                         ++ escBoldStart ++ string ++ escBoldEnd
                                       return y
 
-                                   where escBoldStart = if bold then "\ESC[2m" else ""
+                                   where escBoldStart = if bold then "\ESC[1m" else ""
                                          escBoldEnd = if bold then "\ESC[m" else ""
 
 screenPos :: (Int, Int) -> (TPosX, TPosY) -> Int -> Int -> Int -> Int -> (Int, Int)
-screenPos (x, y) (posX, posY) width height padX padY = (x + padX + 1
-                                                          + tPosX * (width * 2 + padX),
-                                                        y + padY
-                                                          + tPosY * (height + padY))
+screenPos (x, y) (posX, posY) width height padX padY = (x + padX + 1 + tPosX * (width * 2 + padX),
+                                                        y + padY + tPosY * (height + padY))
                                          where tPosX = fromEnum posX
                                                tPosY = fromEnum posY
-
 logDown :: String -> Int -> IO ()
 logDown string prevY = do
 
@@ -193,12 +222,8 @@ getAllPositions = [ (a,b) | a <- [(PX0)..], b <- [(PY0)..] ]
 drawBoard :: (Int, Int) -> Int -> Int -> Int -> Int -> IO Int
 drawBoard (x, y) sWidth sHeight padX padY = return =<< foldlM (\acc curr -> do
     
-  return =<< drawBox (screenPos (x, y) (curr)
-                                sWidth sHeight
-                                padX padY)
-                      sWidth sHeight
-                      (padY /= 0) (isTopRow curr)
-                      False) 0 getAllPositions
+  return =<< drawBox (screenPos (x, y) (curr) sWidth sHeight padX padY)
+                     sWidth sHeight padX padY (isTopRow curr) False) 0 getAllPositions
 
 isTopRow :: (TPosX, TPosY) -> Bool
 isTopRow (_,PY0) = True
@@ -214,6 +239,8 @@ data TPlay = PL1 | PL2 deriving (Eq, Show)
 data TMove = TMove (TPosX, TPosY) TPlay deriving (Eq, Show)
 
 data TGameState = Empty | Node TMove ([TGameState]) deriving (Eq, Show)
+
+data Direction = DLeft | DRight | DUp | DDown deriving (Eq, Show)
 
         -- ******************************** GAME LOGIC ******************************** --
 
