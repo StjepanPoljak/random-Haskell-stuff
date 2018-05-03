bd7e00fd4cbfb4a10766cfbe985a4448cea3fb83
Stjepan Poljak <spoljak@seahost.dominion>
2018-05-03 23:03:15 +0200

    Some game logic changes. Started working on game loop.

--- TicTacToeGame/TicTacToe.hs
+++ TicTacToeGame/TicTacToe.hs
@@ -3,21 +3,6 @@ import System.Process
 import Control.Monad.State
 import System.IO
 
-gOrigin :: (Int, Int)
-gOrigin = (10, 1)
-
-gWidth :: Int
-gWidth = 3
-
-gHeight :: Int
-gHeight = 4
-
-gPaddingX :: Int
-gPaddingX = 0
-
-gPaddingY :: Int
-gPaddingY = 0
-
 main :: IO ()
 main = do
 
@@ -34,6 +19,45 @@ main = do
     printStringOn (0,0) "Thank you for playing! Bye!\n" False
 
     return ()
+
+programLoop :: IO ()
+programLoop = do
+  player <- choosePlayer
+  drawAll
+  waitForInput [] (PX0, PY0)
+  return ()
+
+        -- ****************************** GUI SETTINGS ******************************** --
+
+gOrigin :: (Int, Int)
+gOrigin = (10, 5)
+
+gWidth :: Int
+gWidth = 3
+
+gHeight :: Int
+gHeight = 3
+
+gPaddingX :: Int
+gPaddingX = 0
+
+gPaddingY :: Int
+gPaddingY = 0
+
+        -- ******************************** GUI DATA ********************************** --
+
+data TPosX = PX0 | PX1 | PX2 deriving (Eq, Show, Enum)
+data TPosY = PY0 | PY1 | PY2 deriving (Eq, Show, Enum)
+
+data Direction = DLeft | DRight | DUp | DDown deriving (Eq)
+
+instance Show Direction where
+  show DUp = "Up"
+  show DDown = "Down"
+  show DLeft = "Left"
+  show DRight = "Right"
+
+        -- ******************************** GUI ******************************** --
 
 drawAll :: IO ()
 drawAll = do
@@ -56,102 +80,6 @@ drawAll = do
                                 -- write on bottom of screen and then get
                                 -- cursor back where it was - for now we don't
                                 -- need this in our program...
-
-        -- **************************** KEYBOARD **************************** --
-
-waitForInput :: [Char] -> (TPosX, TPosY) -> IO ()
-waitForInput prev pos = do
-    
-     x <- stdin `ifReadyDo` getChar
-     takeAction x prev pos
-
-move :: Direction -> (TPosX, TPosY) -> (Int, Int) -> Int -> Int -> Int -> Int -> IO (TPosX, TPosY)
-move dir pos origin width height padX padY = do
-
-                                    logDown (show dir) 0
-
-                                    drawBox realPos width height
-                                            padX padY (isTopRow pos)
-                                            False
-
-                                    drawBox newPos width height
-                                            padX padY (isTopRow new)
-                                            True
-
-                                    return new
-        
-        where realPos = screenPos gOrigin pos gWidth gHeight gPaddingX gPaddingY
-              newPos = screenPos origin new width height padX padY
-              new = (go pos dir)
-
-moveConv :: Direction -> (TPosX, TPosY) -> IO (TPosX, TPosY)
-moveConv dir pos = move dir pos gOrigin gWidth gHeight gPaddingX gPaddingY
-
-takeAction :: Maybe Char -> [Char] -> (TPosX, TPosY) -> IO ()
-takeAction Nothing prev pos
-
-        | prev == ['\ESC']           = return ()
-        | prev == ['A','[','\ESC']   = waitForInput [] =<< moveConv DUp pos
-        | prev == ['B','[','\ESC']   = waitForInput [] =<< moveConv DDown pos
-        | prev == ['C','[','\ESC']   = waitForInput [] =<< moveConv DRight pos
-        | prev == ['D','[','\ESC']   = waitForInput [] =<< moveConv DLeft pos
-        | prev == ['\n']             = do { logDown "Enter!" 0; waitForInput [] pos }
-        | otherwise                  = waitForInput [] pos
-
-takeAction (Just char) prev pos = waitForInput (char:prev) pos
-
-ifReadyDo :: Handle -> IO a -> IO (Maybe a)
-ifReadyDo hnd x = hReady hnd >>= f
-   where f True = x >>= return . Just
-         f _    = return Nothing
-
-incX :: TPosX -> TPosX
-incX x = if x == last [(PX0)..] then x else succ x
-
-incY :: TPosY -> TPosY
-incY y = if y == PY0 then y else pred y
-
-decX :: TPosX -> TPosX
-decX x = if x == PX0 then x else pred x
-
-decY :: TPosY -> TPosY
-decY y = if y == last [(PY0)..] then y else succ y
-
-go :: (TPosX, TPosY) -> Direction -> (TPosX, TPosY)
-go (x, y) DUp = (x, incY y)
-go (x, y) DRight = (incX x, y)
-go (x, y) DLeft = (decX x, y)
-go (x, y) DDown = (x, decY y)
-
-        -- ***************************** TERMINAL ***************************** --
-
-isNumeric :: Char -> Bool
-isNumeric char = char `elem` "0123456789"
-
-readFromTerminal :: String -> Int
-readFromTerminal string = foldl (\acc x -> if isNumeric x
-                                           then
-                                             (acc * 10 + (read [x]))
-                                           else acc) 0 string
-getColumns :: IO Int
-getColumns = do
-     columnsRaw <- (readProcess "tput" ["cols"] [])
-     let columns = readFromTerminal columnsRaw
-     return columns
-
-getRows :: IO Int
-getRows = do
-     rowsRaw <- (readProcess "tput" ["lines"] [])
-     let rows = readFromTerminal rowsRaw
-     return rows
-
-hideCursor :: IO ()
-hideCursor = callProcess "tput" ["civis"]
-
-showCursor :: IO ()
-showCursor = callProcess "tput" ["cnorm"]
-
-        -- ******************************** GUI ******************************** --
 
 clearScreen :: IO ()
 clearScreen = putStrLn "\ESC[2J"
@@ -227,69 +155,200 @@ isTopRow :: (TPosX, TPosY) -> Bool
 isTopRow (_,PY0) = True
 isTopRow _ = False
 
-        -- ********************************** DATA ************************************ --
+lengthWithoutEsc :: String -> Int
+lengthWithoutEsc string = fst $ foldl (\acc x -> if x == '\ESC'
+                                                 then
+                                                   (fst acc, True)
+                                                 else
+                                                   if snd acc
+                                                   then
+                                                     (fst acc, not $ isLetter x)
+                                                   else
+                                                     (fst acc + 1, False))
+                                      (0,False) string
 
-data TPosX = PX0 | PX1 | PX2 | PX3 deriving (Eq, Show, Enum)
-data TPosY = PY0 | PY1 deriving (Eq, Show, Enum)
+isLetter :: Char -> Bool
+isLetter char = char `elem` (['A'..'Z'] ++ ['a'..'z'])
+
+        -- **************************** KEYBOARD **************************** --
+
+waitForInput :: [Char] -> (TPosX, TPosY) -> IO ()
+waitForInput prev pos = do
+    
+     x <- stdin `ifReadyDo` getChar
+     takeAction x prev pos
+
+move :: Direction -> (TPosX, TPosY) -> (Int, Int) -> Int -> Int
+                  -> Int -> Int -> IO (TPosX, TPosY)
+move dir pos origin width height padX padY = do
+
+                                    logDown (show dir) 0
+
+                                    drawBox realPos width height
+                                            padX padY (isTopRow pos)
+                                            False
+
+                                    drawBox newPos width height
+                                            padX padY (isTopRow new)
+                                            True
+
+                                    return new
+        
+        where realPos = screenPos gOrigin pos gWidth gHeight gPaddingX gPaddingY
+              newPos = screenPos origin new width height padX padY
+              new = (go pos dir)
+
+moveConv :: Direction -> (TPosX, TPosY) -> IO (TPosX, TPosY)
+moveConv dir pos = move dir pos gOrigin gWidth gHeight gPaddingX gPaddingY
+
+takeAction :: Maybe Char -> [Char] -> (TPosX, TPosY) -> IO ()
+takeAction Nothing prev pos
+
+    | prev == ['\ESC']           = return ()
+    | prev == ['A','[','\ESC']   = waitForInput [] =<< moveConv DUp pos
+    | prev == ['B','[','\ESC']   = waitForInput [] =<< moveConv DDown pos
+    | prev == ['C','[','\ESC']   = waitForInput [] =<< moveConv DRight pos
+    | prev == ['D','[','\ESC']   = waitForInput [] =<< moveConv DLeft pos
+    | prev == ['\n']             = do { logDown "Enter!" 0; drawCenterSymbol pos 'X'; waitForInput [] pos }
+    | otherwise                  = waitForInput [] pos
+
+takeAction (Just char) prev pos = waitForInput (char:prev) pos
+
+ifReadyDo :: Handle -> IO a -> IO (Maybe a)
+ifReadyDo hnd x = hReady hnd >>= f
+   where f True = x >>= return . Just
+         f _    = return Nothing
+
+incX :: TPosX -> TPosX
+incX x = if x == last [(PX0)..] then x else succ x
+
+incY :: TPosY -> TPosY
+incY y = if y == PY0 then y else pred y
+
+decX :: TPosX -> TPosX
+decX x = if x == PX0 then x else pred x
+
+decY :: TPosY -> TPosY
+decY y = if y == last [(PY0)..] then y else succ y
+
+go :: (TPosX, TPosY) -> Direction -> (TPosX, TPosY)
+go (x, y) DUp = (x, incY y)
+go (x, y) DRight = (incX x, y)
+go (x, y) DLeft = (decX x, y)
+go (x, y) DDown = (x, decY y)
+
+        -- ***************************** TERMINAL ***************************** --
+
+isNumeric :: Char -> Bool
+isNumeric char = char `elem` "0123456789"
+
+readFromTerminal :: String -> Int
+readFromTerminal string = foldl (\acc x -> if isNumeric x
+                                           then
+                                             (acc * 10 + (read [x]))
+                                           else acc) 0 string
+getColumns :: IO Int
+getColumns = do
+     columnsRaw <- (readProcess "tput" ["cols"] [])
+     let columns = readFromTerminal columnsRaw
+     return columns
+
+getRows :: IO Int
+getRows = do
+     rowsRaw <- (readProcess "tput" ["lines"] [])
+     let rows = readFromTerminal rowsRaw
+     return rows
+
+hideCursor :: IO ()
+hideCursor = callProcess "tput" ["civis"]
+
+showCursor :: IO ()
+showCursor = callProcess "tput" ["cnorm"]
+
+        -- **************************** GAME SPECIFIC GUI ***************************** --
+
+choosePlayer :: IO TPlay
+choosePlayer = do
+      player <- getLine
+      return PL1
+
+getSymbolForPlayer :: TPlay -> Char
+getSymbolForPlayer n = case n of
+                         PL1 -> 'X'
+                         PL2 -> 'O'
+
+drawCenterSymbol :: (TPosX, TPosY) -> Char -> IO ()
+drawCenterSymbol tetrisPos symbol = do
+
+  printStringOn (realX + (gWidth + 1) - 1, realY + (gHeight + 1) `div` 2) [symbol] False
+  return ()
+
+    where (realX, realY) = screenPos gOrigin tetrisPos gWidth gHeight gPaddingX gPaddingY
+
+        -- ******************************** GAME DATA ********************************* --
 
 data TPlay = PL1 | PL2 deriving (Eq, Show)
+
+data TPlayType = Human | AI deriving (Eq, Show)
 
 data TMove = TMove (TPosX, TPosY) TPlay deriving (Eq, Show)
 
 data TGameState = Empty | Node TMove ([TGameState]) deriving (Eq, Show)
 
-data Direction = DLeft | DRight | DUp | DDown deriving (Eq, Show)
-
         -- ******************************** GAME LOGIC ******************************** --
 
-addMove :: TMove -> State TGameState (Maybe TMove)
-addMove newMove = do
-          nodes <- get
-          put (newMove +> nodes)
-          return Nothing
+getPlayer :: TMove -> TPlay
+getPlayer (TMove _ player) = player
+
+getLastPlayer :: TGameState -> TPlay
+getLastPlayer (Node move _) = getPlayer move
 
 (>+) :: TMove -> (Maybe [TMove]) -> (Maybe [TMove])
-move >+ Nothing = Just [move]
+move >+ Nothing = Nothing
 move >+ (Just moves) = if move `elem` moves
                        then
                          Nothing
                        else
                          Just (move:moves)
 
-(+>) :: TMove -> TGameState -> TGameState
-move +> Empty = Node move []
-move +> nodes = (Node move [nodes])
+-- add move as a new tree level --
+(+>) :: TMove -> Maybe TGameState -> Maybe TGameState
+move +> Nothing = Nothing
+move +> (Just Empty) = Just $ Node move []
+move +> (Just nodes) = if getPlayer move == getLastPlayer nodes then Nothing else Just $ (Node move [nodes])
 
-(>-) :: TMove -> TGameState -> TGameState
-move >- Empty = move +> Empty
-move >- (Node lastMove list) = (Node lastMove $ (Node move []):list)
+-- add move as a new mode in the same level --
+(>-) :: TMove -> Maybe TGameState -> Maybe TGameState
+move >- Nothing = Nothing
+move >- (Just Empty) = move +> (Just Empty)
+move >- Just (Node lastMove list) = Just (Node lastMove $ (Node move []):list)
 
-accMoves :: TGameState -> (Maybe [TMove])
-accMoves Empty = Nothing
-accMoves (Node move []) = Just [move]
-accMoves (Node move [node]) = move >+ (accMoves node)
-accMoves (Node move list) = Nothing
+accMoves :: Maybe TGameState -> (Maybe [TMove])
+accMoves Nothing = Nothing
+accMoves (Just Empty) = Just []
+accMoves (Just (Node move [])) = Just [move]
+accMoves (Just (Node move [node])) = move >+ (accMoves (Just node))
+accMoves (Just (Node move list)) = Nothing
 
-traverseMoves :: TGameState -> (Maybe TMove)
-traverseMoves Empty = Nothing
-traverseMoves (Node move []) = Just move
-traverseMoves (Node move [node]) = traverseMoves node
-traverseMoves (Node move list) = Nothing
-
-getLastMove :: State TGameState (Maybe TMove)
-getLastMove = do
-      lastState <- get
-      return (traverseMoves lastState)
+traverseMoves :: Maybe TGameState -> (Maybe TMove)
+traverseMoves Nothing = Nothing
+traverseMoves (Just Empty) = Nothing
+traverseMoves (Just (Node move [])) = Just move
+traverseMoves (Just (Node move [node])) = traverseMoves (Just node)
+traverseMoves (Just (Node move list)) = Nothing
 
 switchPlayers :: TPlay -> TPlay
 switchPlayers PL1 = PL2
 switchPlayers PL2 = PL1
 
---generatePossibleMoves :: TGameState -> TPlay -> Int -> (Maybe TGameState)
---generatePossibleMoves state player 0 = Just state
---generatePossibleMoves state player level = foldl (\acc x -> if not $ x `elem` disallowed then (generatePossibleMoves (x >- state) (switchPlayers player) (level - 1)) else acc) (Just Empty) getAllPositions
---                      where Just disallowed = (accMoves state)
+getPossibleMoves :: Maybe TGameState -> TPlay -> Maybe [TMove]
+getPossibleMoves Nothing _ = Nothing
+getPossibleMoves state player = Just $ (map (\pos -> TMove pos player) . filter (\pos -> if moves == Nothing then False else let Just movesuw = moves in not $ pos `elem` (map getPositionFromMove movesuw) )) getAllPositions
+           where moves = accMoves state
+           
+mockGameState :: Maybe TGameState -> TPlay -> Maybe TGameState
+mockGameState Nothing _ = Nothing
+mockGameState state player = let possMoves = (getPossibleMoves state player) in if possMoves == Nothing then state else let Just possMovesUW = possMoves in foldl (\acc x -> (mockGameState (x +> acc) (switchPlayers player))) (state) possMovesUW
 
-
---decide :: TGameState -> TPlayer -> Int -> TMove
---decide (Node move nodes) = 
+getPositionFromMove :: TMove -> (TPosX, TPosY)
+getPositionFromMove (TMove pos play) = pos
